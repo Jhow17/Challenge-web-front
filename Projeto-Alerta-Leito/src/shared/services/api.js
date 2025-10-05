fce55d105e4d1e8582861a7aa66e181eb91f10e8
Jhow17 <silvajonathan234@gmail.com>
2025-10-05 10:39:42 -0300

    atualiza a baseURL

--- Projeto-Alerta-Leito/src/shared/services/api.js
+++ Projeto-Alerta-Leito/src/shared/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  //baseURL: 'https://challenge-web-front.onrender.com'
-  baseURL: 'http://192.168.0.20:5001'
+  baseURL: 'https://challenge-web-front.onrender.com'
+ 
 })
 export default api
