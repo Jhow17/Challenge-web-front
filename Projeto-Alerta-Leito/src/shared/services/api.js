84878d15c5ad940beeb86035b2ba5f79bb3f716d
Jhow17 <silvajonathan234@gmail.com>
2025-11-09 17:56:04 -0300

    fix rewrite problem 404

--- Projeto-Alerta-Leito/src/shared/services/api.js
+++ Projeto-Alerta-Leito/src/shared/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: 'https://challenge-web-front.onrender.com'
-  //baseURL: 'http://192.168.0.20:5001'
+  baseURL:import.meta.env.VITE_SERVER_URL
+  //baseURL: 'http://192.168.0.17:5001'
  
 })
 export default api
